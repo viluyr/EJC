a056fa18bb076538f0f06d94538ae651fb634d76
Denis <viluyr@gmail.com>
2017-06-22 14:30:54 +0300

    Hello World

---
+++ scr/main/java/Main.java
@@ -0,0 +1,10 @@
+import static java.lang.System.out;
+
+/**
+ * Created by Денис on 22.06.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Hello! ");
+    }
+}
