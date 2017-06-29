8befe0e7e10cfea1c3ee9b8b4736eb1f8120bf42
Denis <viluyr@gmail.com>
2017-06-29 15:21:35 +0300

    Ducs Casino with Pattern Strategy  [first part /not completed]

--- scr/main/java/Task_02/FlyNoWay.java
+++ scr/main/java/Task_02/FlyNoWay.java
@@ -3,6 +3,6 @@ package Task_02;
 
 public class FlyNoWay implements FlyBehavior {
     public void fly(){
-        System.out.println("Can't Fly");
+        System.out.println(" I Can't Fly");
     }
 }
